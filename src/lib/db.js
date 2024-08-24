de836899f34e7689522d55d79263e2b588ed4075
FameILY <bhambidsatyam@gmail.com>
2024-08-24 17:52:39 +0530

    console logged uri

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 const MONGO_URI = process.env.MONGO_URI;
+console.log(MONGO_URI)
 
 if (!MONGO_URI) {
   throw new Error('Please define the MONGO_URI environment variable inside .env.local');
